15baa94d99a92ce87f7d6a9b1982722559963fdb
sophiaecl <67115618+sophiaecl@users.noreply.github.com>
2025-03-26 17:57:35 +0100

    fix 2

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -81,7 +81,7 @@ export const authService = {
     formData.append('age', userData.age);
     formData.append('gender', userData.gender);
     
-    return axios.post(`/auth/register`, formData);
+    return axios.post(`https://packwise-backend-580624387675.europe-southwest1.run.app/auth/register`, formData);
   },
   
   // Login user
@@ -90,7 +90,7 @@ export const authService = {
     formData.append('username', credentials.username);
     formData.append('password', credentials.password);
     
-    const response = await axios.post(`/auth/token`, formData);
+    const response = await axios.post(`https://packwise-backend-580624387675.europe-southwest1.run.app/auth/token`, formData);
     if (response.data.access_token) {
       localStorage.setItem('token', response.data.access_token);
     }
